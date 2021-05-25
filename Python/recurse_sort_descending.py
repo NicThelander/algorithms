c50e945e73dd32f833126c32b0b6a236270b9df6
learningConstantly <nic.thelander@gmail.com>
2021-05-25 10:22:42 +0200

    More documentation updates

--- Python/recurse_sort_descending.py
+++ Python/recurse_sort_descending.py
@@ -1,6 +1,19 @@
 def recurse_sort_des(ls, active_index=0):
-    """recursively sorts a list from biggest to smallest.
-    will both update the original list and return the result of the processing."""
+    """
+    Passed a list as an argument and then recursively
+    finds and swaps the biggest integer from further in
+    the list with the current position.
+
+    Note: The active index is used for tracking it's place
+    within the list, you can supply one yourself but it will
+    only process from that index onwards then.
+
+    :param ls: The list that should be passed to the function
+    :param active_index: To keep track of the index position
+    being worked on, you can supply your own one to declare
+    where in the list it should start.
+    :return: Returns the sorted list from biggest to smallest
+    """
     swap_val = ls[active_index]
     swap_index = active_index
     if len(ls) - 1 > active_index:
